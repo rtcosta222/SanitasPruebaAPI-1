896853be3e6d9f913e01e37dbc626b98718f7713
rosselli.trindade <rosselli.trindade@1trindadeX650G2.babel-si.local>
2022-06-17 12:42:18 +0200

    1st try.

---
+++ src/main/java/com/bi/springboot/di/app/SanitasPruebaApi1Application.java
@@ -0,0 +1,13 @@
+package com.bi.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitasPruebaApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitasPruebaApi1Application.class, args);
+	}
+
+}
